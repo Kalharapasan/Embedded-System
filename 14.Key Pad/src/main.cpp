04c8da3f573f8829a719b32e6ac7f6c0d0c8f7e9
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-12 17:45:32 +0530

    Key Pad Example

--- 14.Key Pad/src/main.cpp
+++ 14.Key Pad/src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Keypad.h>
+
+const uint8_t ROWS = 4;
+const uint8_t COLS = 4;
+char keys[ROWS][COLS] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
+
+uint8_t colPins[COLS] = { 5, 4, 3, 2 }; // Pins connected to C1, C2, C3, C4
+uint8_t rowPins[ROWS] = { 9, 8, 7, 6 }; // Pins connected to R1, R2, R3, R4
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  char key = keypad.getKey();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+  if (key != NO_KEY) {
+    Serial.println(key);
+  }
