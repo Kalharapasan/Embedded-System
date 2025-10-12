0701aeeac5e7cb796564314f90cfd788d4440b8f
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-12 16:12:51 +0530

    LiquidCrystal_I2C HelloWorld Text

--- 07.LiquidCrystal_I2C_HelloWorld/src/main.cpp
+++ 07.LiquidCrystal_I2C_HelloWorld/src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <LiquidCrystal_I2C.h>
+
+#define I2C_ADDR    0x27
+#define LCD_COLUMNS 20
+#define LCD_LINES   4
+
+LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Init
+  lcd.init();
+  lcd.backlight();
+
+  // Print something
+  lcd.setCursor(3, 0);
+  lcd.print("Hello, world!");
+  lcd.setCursor(2, 1);
+  lcd.print("Wokwi Online IoT");
+  lcd.setCursor(5, 2);
+  lcd.print("Simulator");
+  lcd.setCursor(7, 3);
+  lcd.print("Enjoy!");
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
