105cda583b303e5daab83bc13e0d6d0b348877a7
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-12 11:36:40 +0530

    Led Connect Text

--- 01.LED Connect/src/main.cpp
+++ 01.LED Connect/src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int LEDPIN =13;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode (LEDPIN,OUTPUT);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  digitalWrite(LEDPIN,HIGH);
+  delay(500);
+  digitalWrite(LEDPIN,LOW);
+  delay(500);
+  
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
