66cb964422f4cbe6ab06c7c3ea3b24fbc0223e5a
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-17 12:42:42 +0530

    A Virtual Tape Measure

--- 19.Ultrasonic Sensor with Buzzer/src/main.cpp
+++ 19.Ultrasonic Sensor with Buzzer/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+#define echoPin 8
+#define trigPin 10
+const int buzzer = 9;
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+long duration;
+int distance;
+
+void setup() 
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzer, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Ultrasonic Sensor HC-SRC04 Test");
+  Serial.println("with Arduino UNO R3");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() 
+{
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  if(distance < 5){
+    tone(buzzer, 500);
+    delay(1000);
+    noTone(buzzer);
+  }
 }
