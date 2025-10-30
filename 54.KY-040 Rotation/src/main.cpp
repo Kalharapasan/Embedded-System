3edad33538f535192e15cd57c98a3250bb8a412f
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-31 01:02:16 +0530

    KY-040 Rotation

--- 54.KY-040 Rotation/src/main.cpp
+++ 54.KY-040 Rotation/src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#define ENCODER_CLK 2
+#define ENCODER_DT  3
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(ENCODER_CLK, INPUT);
+  pinMode(ENCODER_DT, INPUT);
 }
+
+int lastClk = HIGH;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int newClk = digitalRead(ENCODER_CLK);
+  if (newClk != lastClk) {
+    lastClk = newClk;
+    int dtValue = digitalRead(ENCODER_DT);
+    if (newClk == LOW && dtValue == HIGH) {
+      Serial.println("Rotated clockwise ⏩");
+    }
+    if (newClk == LOW && dtValue == LOW) {
+      Serial.println("Rotated counterclockwise ⏪");
+    }
+  }
 }
