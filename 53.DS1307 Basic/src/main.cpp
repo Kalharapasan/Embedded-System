849f75381c038d36f427988f4a04cddf9101b430
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-31 00:54:38 +0530

    DS1307 Basic

--- 53.DS1307 Basic/src/main.cpp
+++ 53.DS1307 Basic/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+#include "RTClib.h"
 
-// put function declarations here:
-int myFunction(int, int);
+RTC_DS1307 rtc;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+void setup () {
+  Serial.begin(115200);
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop () {
+  DateTime now = rtc.now();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print("Current time: ");
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+
+
+  Serial.println();
+  delay(3000);
 }
