7dc053f559173c901aabe8de87d4a4f2f486b87a
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-31 02:23:56 +0530

    Arduino QR Code Generator

--- 62.Arduino QR Code Generator/src/main.cpp
+++ 62.Arduino QR Code Generator/src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
+#include <LedControl.h> 
+#include <qrcode.h> 
 
-// put function declarations here:
-int myFunction(int, int);
+int size = 6;
+int side = size * 8;
+QRCode qrcode;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+LedControl ledControls[] = {
+  LedControl(0, 1, 2, size),
+  LedControl(3, 4, 5, size),
+  LedControl(6, 7, 8, size),
+  LedControl(9, 10, 11, size),
+  LedControl(A0, A1, A2, size),
+  LedControl(A3, A4, A5, size),
+};
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+int main() {
+  int version = 1;
+  uint8_t qrcodeData[qrcode_getBufferSize(version)];
+  qrcode_initText(&qrcode, qrcodeData, version, ECC_LOW, "HUBELBAUER.NET");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int scale = 2;
+  int shift = (side - (qrcode.size * scale)) / 2;
+  for (int y = shift; y < side; y++) {
+    for (int x = shift; x < side; x++) {
+      int index = y / 8;
+      int address = size - 1 - (x / 8);
+      int row = y % 8;
+      int column = 7 - (x % 8);
+      bool state = false;
+      if (x >= shift && x < shift + (qrcode.size * scale) && y >= shift && y < shift + (qrcode.size * scale)) {
+        state = qrcode_getModule(&qrcode, (x - shift) / scale, (y - shift) / scale);
+      }
+
+      ledControls[index].setLed(address, row, column, state);
+    }
+  }
 }
