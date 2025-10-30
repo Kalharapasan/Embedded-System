5d2d20dbb33d21bd06a62acbd18a8458ea65639b
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-31 00:44:37 +0530

    Arduino Pind Example

--- 52.Arduino Pind Example/src/main.cpp
+++ 52.Arduino Pind Example/src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  lcd.init();
+  lcd.backlight();
+  lcd.println("PIND value:");
+  pinMode(0, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
 }
 
+int value = -1;
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (PIND != value) {
+    lcd.setCursor(6, 1);
+    lcd.print(PIND);
+    lcd.print("   ");
+    value = PIND;
+  }
 }
