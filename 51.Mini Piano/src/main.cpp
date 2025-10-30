8d0b20e1dfcdc1dd06466d4b5734ec74c6c5a902
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-31 00:42:57 +0530

    Arduino Pind Example

--- 51.Mini Piano/src/main.cpp
+++ 51.Mini Piano/src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
+#include "pitches.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define SPEAKER_PIN 8
+
+const uint8_t buttonPins[] = { 12, 11, 10, 9, 7, 6, 5, 4 };
+const int buttonTones[] = {
+  NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4,
+  NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5
+};
+const int numTones = sizeof(buttonPins) / sizeof(buttonPins[0]);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  for (uint8_t i = 0; i < numTones; i++) {
+    pinMode(buttonPins[i], INPUT_PULLUP);
+  }
+  pinMode(SPEAKER_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int pitch = 0;
+  for (uint8_t i = 0; i < numTones; i++) {
+    if (digitalRead(buttonPins[i]) == LOW) {
+      pitch = buttonTones[i];
+    }
+  }
+  if (pitch) {
+    tone(SPEAKER_PIN, pitch);
+  } else {
+    noTone(SPEAKER_PIN);
+  }
 }
