f42cd61bbd703224327220a394ec1df27fa1ab0a
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-31 01:11:27 +0530

    KY-040 Rotation

--- 55.KY-040 Counter/src/main.cpp
+++ 55.KY-040 Counter/src/main.cpp
@@ -1,18 +1,53 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-// put function declarations here:
-int myFunction(int, int);
+#define ENCODER_CLK 2
+#define ENCODER_DT  3
+#define ENCODER_SW  4
+
+int counter = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  lcd.init();
+  lcd.backlight();
+  pinMode(ENCODER_CLK, INPUT);
+  pinMode(ENCODER_DT, INPUT);
+  pinMode(ENCODER_SW, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_CLK), readEncoder, FALLING);
+}
+
+void readEncoder() {
+  int dtValue = digitalRead(ENCODER_DT);
+  if (dtValue == HIGH) {
+    counter++; 
+  }
+  if (dtValue == LOW) {
+    counter--; 
+  }
+}
+int getCounter() {
+  int result;
+  noInterrupts();
+  result = counter;
+  interrupts();
+  return result;
+}
+
+void resetCounter() {
+  noInterrupts();
+  counter = 0;
+  interrupts();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  lcd.setCursor(3, 0);
+  lcd.print("Counter:");
+  lcd.setCursor(7, 1);
+  lcd.print(getCounter());
+  lcd.print("        ");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (digitalRead(ENCODER_SW) == LOW) {
+    resetCounter();
+  }
 }
