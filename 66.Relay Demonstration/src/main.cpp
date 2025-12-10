eb3534977c7507b5ee9388da39ee6be5800b16f1
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-10 23:32:09 +0530

    Relay Demonstration

--- 66.Relay Demonstration/src/main.cpp
+++ 66.Relay Demonstration/src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
+const int relay1Pin = 5;
+const int relay2Pin = 4;
+const int relay3Pin = 3;
+const int relay4Pin = 2;
 
-// put function declarations here:
-int myFunction(int, int);
+void setup() 
+{
+  pinMode(relay1Pin, OUTPUT);
+  pinMode(relay2Pin, OUTPUT);
+  pinMode(relay3Pin, OUTPUT);
+  pinMode(relay4Pin, OUTPUT);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  delay(1000);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() 
+{
+  digitalWrite(relay1Pin,HIGH); 
+  delay(1000);
+  digitalWrite(relay1Pin,LOW);  
+  delay(1000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(relay2Pin,HIGH);
+  delay(1000);
+  digitalWrite(relay2Pin,LOW);
+  delay(1000);
+
+  digitalWrite(relay3Pin,HIGH);
+  delay(1000);
+  digitalWrite(relay3Pin,LOW);
+  delay(1000);
+
+  digitalWrite(relay4Pin,HIGH);
+  delay(1000);
+  digitalWrite(relay4Pin,LOW);
+  delay(1000);
 }
