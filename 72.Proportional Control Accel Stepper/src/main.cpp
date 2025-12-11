39a8918b095717f9c72ba8fef1808f6f319849ca
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-11 21:22:33 +0530

    Proportional Control Accel Stepper

--- 72.Proportional Control Accel Stepper/src/main.cpp
+++ 72.Proportional Control Accel Stepper/src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#include <AccelStepper.h>
+AccelStepper stepper; 
+const byte PositionPot = A0;
+const byte AccelerationPot = A1;
+void setup()
+{  
+  stepper.setMaxSpeed(1000);
+  stepper.setAcceleration(35);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  int analog_in = analogRead(PositionPot);
+  stepper.setAcceleration(analogRead(AccelerationPot));
+  stepper.moveTo(analog_in);
+  stepper.run();
 }
