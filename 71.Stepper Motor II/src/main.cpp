5f974eaa20529b1b29063d5483d7ce2b59787143
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-11 21:12:02 +0530

    Stepper Motor II

--- 71.Stepper Motor II/src/main.cpp
+++ 71.Stepper Motor II/src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#define Ap 10 
+#define Am 11  
+#define Bp 9   
+#define Bm 8  
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(Ap,OUTPUT);  pinMode(Am,OUTPUT);
+  pinMode(Bp,OUTPUT);  pinMode(Bm,OUTPUT);
 }
 
+int I = 0 ;
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if ( I++ <25 ) {  
+    delay(10);
+    digitalWrite(Bm,LOW) ;  digitalWrite(Ap,HIGH);
+    delay(10);
+    digitalWrite(Ap,LOW) ;  digitalWrite(Bp,HIGH);
+    delay(10);
+    digitalWrite(Bp,LOW) ;  digitalWrite(Am,HIGH);
+    delay(10);
+    digitalWrite(Am,LOW) ;  digitalWrite(Bm,HIGH);
+  } else while(1);
 }
